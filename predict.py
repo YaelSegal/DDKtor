6bf2448db4166f69490f377aee4b3e5710b44cdd
YaelSegal <hgkxdk@gmail.com>
2021-09-03 16:56:29 +0300

    add wraping to predict.py

--- predict.py
+++ predict.py
@@ -14,6 +14,7 @@ import random
 from utils import SR, create_textgrid
 import tqdm
 import dataset
+from helpers.utilities import basic_hierarchy_dict
 
 parser = argparse.ArgumentParser(description='test vowel/vot')
 parser.add_argument('--data', type=str, default='./data/processed/' , help="directory of the data",)
@@ -23,7 +24,7 @@ parser.add_argument('--out_dir', type=str, default='./data/out_tg/tmp_parts' , h
 # parser.add_argument('--model', type=str, default='./model_cnn_lstm/data_BUCH_UPDATE_ntype_cnn_sim_lr_0.0001_input_size_256_num_layers_2_hidden_size_256_channels_256_normalize_True_norm_type_z_biLSTM_False_measure_rval_dropout_0.3_class_num_3_sigmoid_False_chain_bandreject,noise_lamda_1.0_59021734.pth', help='directory to save the model')
 parser.add_argument('--model', type=str, default='./model_cnn_lstm/data_BUCH_UPDATE_ntype_lstm_sim_lr_0.0001_input_size_256_num_layers_2_hidden_size_256_channels_256_normalize_True_norm_type_z_biLSTM_True_measure_rval_dropout_0.3_class_num_3_sigmoid_False_chain_bandreject,noise_lamda_1.0_59021734.pth', help='directory to save the model')
 
-
+parser.add_argument('--basic_hierarchy_file', default="./data/raw/all_files/files.txt", type=str, help="features dir")
 parser.add_argument('--cuda', action='store_true', help='use CUDA')
 parser.add_argument('--seed', type=int, default=1245,	help='random seed')
 
@@ -57,36 +58,43 @@ path_model = args.model
 test_model, normalize, sigmoid, norm_type  = load_model(path_model) 
 if args.cuda:
     test_model = test_model.cuda()
-
+hierarchy_dict = basic_hierarchy_dict(args.basic_hierarchy_file)
 with torch.no_grad():
     test_model.eval()
     files_list = glob.glob(args.data + "/*.wav")
     for wav_filename in tqdm.tqdm(files_list):
-        wav_basename = wav_filename.split("/")[-1]
+        try:
+            wav_basename = wav_filename.split("/")[-1]
 
-        pred_dataset = dataset.PredictDataset(wav_filename,args.seed, slices_size=1000, overlap=0, normalize=normalize, norm_type=norm_type)
-        pred_loader = torch.utils.data.DataLoader( pred_dataset, batch_size=100, shuffle=False,
-        num_workers=0, pin_memory=args.cuda, collate_fn= dataset.PadCollatePred(dim=0))
+            pred_dataset = dataset.PredictDataset(wav_filename,args.seed, slices_size=1000, overlap=0, normalize=normalize, norm_type=norm_type)
+            pred_loader = torch.utils.data.DataLoader( pred_dataset, batch_size=100, shuffle=False,
+            num_workers=0, pin_memory=args.cuda, collate_fn= dataset.PadCollatePred(dim=0))
+            all_pred_class_idx = []
+            conf = []
+            for batch_idx, (raw, lens_list) in enumerate(pred_loader):
+                raw = raw.to(device)
 
-        all_pred_class_idx = []
-        conf = []
-        for batch_idx, (raw, lens_list) in enumerate(pred_loader):
-            raw = raw.to(device)
+                hidden = test_model.init_hidden(raw.size(0), device)
+                all_outputs = test_model(raw, hidden,lens_list)
+                if len(all_outputs) == 2:
+                    output , hidden = all_outputs
+                else:
+                    output, vector_out, hidden = all_outputs
 
-            hidden = test_model.init_hidden(raw.size(0), device)
-            all_outputs = test_model(raw, hidden,lens_list)
-            if len(all_outputs) == 2:
-                output , hidden = all_outputs
-            else:
-                output, vector_out, hidden = all_outputs
-
-            for idx in range(output.size(0)): 
-                cur_len = lens_list[idx]
-                pred_class_values, pred_class_idx = torch.max(output[idx,:cur_len], dim=1)
-                conf.extend(F.softmax(output[idx,:cur_len], dim=1).cpu().numpy().tolist())
-                pred_class_idx = pred_class_idx.cpu().numpy()
-                pred_class_values = pred_class_values.cpu().numpy()
-                all_pred_class_idx.extend(pred_class_idx)
+                for idx in range(output.size(0)): 
+                    cur_len = lens_list[idx]
+                    pred_class_values, pred_class_idx = torch.max(output[idx,:cur_len], dim=1)
+                    conf.extend(F.softmax(output[idx,:cur_len], dim=1).cpu().numpy().tolist())
+                    pred_class_idx = pred_class_idx.cpu().numpy()
+                    pred_class_values = pred_class_values.cpu().numpy()
+                    all_pred_class_idx.extend(pred_class_idx)
+        except Exception as e:
+            base_filename = os.path.basename(wav_filename)
+            hierarchy_num_array = base_filename.split("_")
+            hierarchy_num_filename = hierarchy_num_array[0]
+            window_number = hierarchy_num_array[1].replace(".wav", "")
+            new_message = "fail to run model prediction on file: {}, window number: {}".format(hierarchy_dict[hierarchy_num_filename], window_number)
+            raise Exception(new_message)
 
         textgrid_basename = wav_basename.replace(".wav", ".TextGrid")
         textgrid_filename = os.path.join(args.out_dir, textgrid_basename)
