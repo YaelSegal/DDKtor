1cb90fb181edd2efc9b598cf3e3e1b7eb24bb2ce
YaelSegal <hgkxdk@gmail.com>
2021-10-21 18:08:13 +0300

    add  error message in prepare_data

--- prepare_data_textgrid.py
+++ prepare_data_textgrid.py
@@ -20,29 +20,33 @@ def process_textgrid_data(raw_dir, output_dir, window_tier):
     voice_start_file_path = os.path.join(raw_dir, VOICE_STARTS_FILENAME)
     files_start_list = open(voice_start_file_path, "w")
     for textgrid_file in tqdm.tqdm(textgrid_list):
-        
-        wav_file = textgrid_file.replace(".TextGrid", ".WAV")
-        if wav_file in all_WAV_files:
-            fix_extention(os.path.join(raw_dir, wav_file))  # convert "WAV" to "wav" if nessecary
-            wav_file = wav_file.replace('WAV', 'wav')
-        else:
-            wav_file = textgrid_file.replace(".TextGrid", ".wav")
-        y , sr = soundfile.read(wav_file)
+        try:
+            wav_file = textgrid_file.replace(".TextGrid", ".WAV")
+            if wav_file in all_WAV_files:
+                fix_extention(os.path.join(raw_dir, wav_file))  # convert "WAV" to "wav" if nessecary
+                wav_file = wav_file.replace('WAV', 'wav')
+            else:
+                wav_file = textgrid_file.replace(".TextGrid", ".wav")
+            y , sr = soundfile.read(wav_file)
 
-        name = wav_file.split("/")[-1].replace(".wav","")
-        textgrid = TextGrid.fromFile(textgrid_file)
-        if not window_tier in textgrid.getNames():
-            raise Exception("window tier {} doesn't exist in {}".format(window_tier, textgrid_file))
-        tier_instances = textgrid.getFirst(window_tier)
-        count = 0
-        for item in tier_instances.intervals:
-            if re.search(r'\S', item.mark):
-                new_wav_filename = output_dir + "/{}_{}.wav".format(name, count)
-                duration_str = "{}/{}_{}.wav,{},{}\n".format(output_dir, name, count, item.minTime,item.maxTime)
-                files_start_list.write(duration_str)
-                soundfile.write(new_wav_filename, y[int(item.minTime*sr):int(item.maxTime*sr)], sr)
-                count += 1   
-
+            name = wav_file.split("/")[-1].replace(".wav","")
+            textgrid = TextGrid.fromFile(textgrid_file)
+            if not window_tier in textgrid.getNames():
+                raise Exception("window tier {} doesn't exist in {}".format(window_tier, textgrid_file))
+            tier_instances = textgrid.getFirst(window_tier)
+            count = 0
+            for item in tier_instances.intervals:
+                if re.search(r'\S', item.mark):
+                    new_wav_filename = output_dir + "/{}_{}.wav".format(name, count)
+                    duration_str = "{}/{}_{}.wav,{},{}\n".format(output_dir, name, count, item.minTime,item.maxTime)
+                    files_start_list.write(duration_str)
+                    soundfile.write(new_wav_filename, y[int(item.minTime*sr):int(item.maxTime*sr)], sr)
+                    count += 1   
+   
+        except Exception as e:
+            print("Error in file {}".format(textgrid_file))
+            raise e
+        print("End process of {}".format(textgrid_file))
     files_start_list.close()
 
 
